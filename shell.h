8c9e6152769b0f4503e7c0639fc32fc5c22c46ba
Ahmed-Mohmed-Sakr <ahmedsakr07407@gmail.com>
2023-05-15 04:22:41 -0700

    update .h file

--- shell.h
+++ shell.h
@@ -1,5 +1,13 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int start_shell(int ac, char **av, char **env);
+char *get_input(char **av);
+
+int _strlen(char *line);
 
 #endif /*SHELL_H*/
