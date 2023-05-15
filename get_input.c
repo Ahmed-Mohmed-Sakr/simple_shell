b8e1ceb1d0a9a9e67fe138e14b22a750fc6cb232
Ahmed-Mohmed-Sakr <ahmedsakr07407@gmail.com>
2023-05-15 03:52:58 -0700

    implement get input function

--- get_input.c
+++ get_input.c
@@ -1,8 +1,26 @@
 #include "shell.h"
 
 /**
+ * get_input - get input to shell exec
+ *
+ * @av: arg values
+ *
+ * Return: line readed;
  */
 
-void get_input()
+char* get_input(char *av)
 {
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t nread;
+
+	nread =  getline(&line, &len, stdin);
+	if (nread == -1)
+	{
+		write(2,av[0],_strlen(av[0]));
+		write(2," :can not read input\n",21);
+		free(line);
+	}
+
+	return (line);
 }
