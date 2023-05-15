987f24c6a750d0462cc516980b794d57fe54a36e
Ahmed-Mohmed-Sakr <ahmedsakr07407@gmail.com>
2023-05-15 04:24:13 -0700

    fixing general problems in the beginning files

--- execute_commands.c
+++ execute_commands.c
@@ -1,4 +1,4 @@
-#include "shell.c"
+#include "shell.h"
 
 /**
  *
--- start_shell.c
+++ start_shell.c
@@ -1,10 +1,32 @@
 #include "shell.h"
 
 /**
+ * start_shell - entry point to start shell
  *
+ * @ac: arguments counter.
+ * @av: arguments values.
+ * @env: environment.
+ *
+ * Return: ending status.
  */
 
-void start_shell()
+int start_shell(int ac, char **av, char **env)
 {
+	char *shell_name = "AZA$ ";
+	char *line = NULL;
+
+	(void)ac;
+	(void)env;
+
+	while (1)
+	{
+		write(1, shell_name, 5);
+
+		line = get_input(av);
+
+		write(1, line, _strlen(line));
+
+		free(line);
+	}
 
 }
