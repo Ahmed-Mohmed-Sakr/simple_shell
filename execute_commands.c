19971ca8f69e42168f598c090c7388202e18b7f6
Ahmed-Mohmed-Sakr <ahmedsakr07407@gmail.com>
2023-05-15 20:18:25 -0700

    init virgion to execute function

--- execute_commands.c
+++ execute_commands.c
@@ -1,9 +1,38 @@
 #include "shell.h"
 
 /**
+ * execute_commands - to execute comands.
  *
+ * @tokens: arguments to execute.
+ * @av: argument values.
+ * @env: environment.
+ *
+ * Return: void.
  */
 
-void execute_commands()
+void execute_commands(char **tokens, char **av, char **env)
 {
+	pid_t child_pid;
+	int stat;
+
+
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		_print(2, av, " :error in creating process");
+		return;
+	}
+
+	if (child_pid == 0)
+	{
+		if (execve(tokens[0], tokens, env) == -1)
+		{
+			_print(2, av, " :error in execute");
+			exit(1);
+		}
+	}
+	else
+	{
+		wait(&stat);
+	}
 }
